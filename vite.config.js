5e07938c6332812fa26f2f99e071eeed4805e5ae
Jahirsf21 <deislhersanchezfunez@gmail.com>
2025-06-09 15:07:29 -0600

    ajustes

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-// vite.config.js
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    // Permite que Vite sea accesible desde cualquier host
-    host: '0.0.0.0', 
-    
-    // Le decimos a Vite que confíe en las peticiones que vienen del dominio de ngrok
-    hmr: {
-      clientPort: 443 // Puerto estándar para HTTPS
-    },
-    
-    // --- AQUÍ ESTÁ LA SOLUCIÓN PRINCIPAL ---
-    // Añadimos el dominio de ngrok a la lista de hosts permitidos.
-    // El punto al principio actúa como un comodín (wildcard).
-    allowedHosts: ['.ngrok-free.app']
-  }
 })
